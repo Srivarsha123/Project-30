9ae06f49035ade83d89136215474c7712d1409af
Srivarsha123 <73740042+Srivarsha123@users.noreply.github.com>
2020-12-17 16:45:51 +0530

    Add files via upload

---
+++ cup.js
@@ -0,0 +1,20 @@
+class Cup extends BaseClass {
+    constructor(x, y){
+      super(x,y,50,50);
+      this.image = loadImage("cup.png");
+      this.visibility = 255;
+    }
+    display(){
+      if(this.body.speed<5){
+      super.display();
+      image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+     }
+     else{
+        push();
+        World.remove(world, this.body)
+        this.visibility = this.visibility - 10
+        tint(255, this.visibility)
+        pop();
+     }
+    }
+  };
